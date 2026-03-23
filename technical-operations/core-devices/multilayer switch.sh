922cd50c6dce9d64bc43dfdbcc073c7db54d4123
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2026-03-24 00:04:31 +0800

    Added Technical Operations Multilayer Switch configuration

---
+++ technical-operations/core-devices/multilayer switch.sh
@@ -0,0 +1,49 @@
+enable
+configure terminal
+
+hostname TO_CORE_MLS
+no ip domain-lookup
+vtp domain TNS-MY-CO
+vtp mode server
+vtp version 2
+vtp password tnsmyco
+
+vlan 10
+name IT
+vlan 20
+name SD
+vlan 99
+name MANAGEMENT
+exit
+
+interface range g1/0/1-2
+switchport mode trunk
+switchport trunk native vlan 99
+switchport trunk allowed vlan 10,20,99
+no shutdown
+
+interface range g01/0/3-4
+channel-group 1 mode active
+exit
+interface port-channel 1
+switchport mode trunk
+switchport trunk native vlan 99
+switchport trunk allowed vlan 10,20,99
+exit
+
+interface range g1/0/5-6
+channel-group 2 mode active
+exit
+interface port-channel 2
+switchport mode trunk
+switchport trunk native vlan 99
+switchport trunk allowed vlan 10,20,99
+exit
+
+interface vlan 99
+ip address 172.16.99.5 255.255.255.224
+no shutdown
+exit
+ip default-gateway 172.16.99.1
+
+do w m
